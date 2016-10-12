6418410d12ef16fd936479d61838267c90baa43b
Ethan Favia <efavia215@live.com>
2016-10-12 13:56:12 -0400

    moved components out of app.js and separated them into their own components

--- public/components/Greeter.jsx
+++ public/components/Greeter.jsx
@@ -1,5 +1,43 @@
-function greeter () {
-    document.write('from greeter function')
-}
+var React = require('react');
+var GreeterMessage = require('./GreeterMessage');
+var GreeterForm = require('./GreeterForm');
 
-module.exports = greeter; 
+var Greeter = React.createClass({
+
+    getDefaultProps: function(){
+        return {
+            name: 'React',
+            message:'This is the default for the message prop'
+        };
+    },
+    getInitialState: function(){
+        return{
+            name: this.props.name,
+            message: this.props.message
+        };
+    },
+    handleNewData: function(updates){
+    
+        this.setState(updates);
+        
+
+        
+    },
+
+    
+    render:function(){
+        var name = this.state.name;
+        var message = this.state.message;
+        return (
+            <div>
+                <GreeterMessage name={name} message={message}/> 
+
+                <GreeterForm onNewData={this.handleNewData}/>
+            </div>
+
+            
+        );
+    }
+});
+
+module.exports = Greeter; 
